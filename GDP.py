ebf07a8ee65021738ad14db9219529f0e97ee2d9
aarohidhand <150023944+aarohidhand@users.noreply.github.com>
2024-08-06 13:45:52 +0530

    Update GDP.py

--- GDP.py
+++ GDP.py
@@ -9,25 +9,23 @@ warnings.filterwarnings('ignore')
 
 URL="https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
 
-tables = pd.read_html(URL)
-df = pd.DataFrame(tables[3])
-df.columns = range(df.shape[1])   #changes title of columns to numbers
+def highest_gdp(URL):
+    tables = pd.read_html(URL)
+    df = pd.DataFrame(tables[3])
+    df.columns = range(df.shape[1])   #changes title of columns to numbers
 
-df = df[[0,2]]
-df = df.iloc[1:11,:]
+    df = df[[0,2]]
+    df = df.iloc[1:11,:]
 
-df.columns = ['Country','GDP (Million USD)']
+    df.columns = ['Country','GDP (Million USD)']
+    
+    df['GDP (Million USD)'] = df['GDP (Million USD)'].astype(int)   #column's datatype is converted to int
+    df['GDP (Million USD)'] = (df[['GDP (Million USD)']]/1000)  #converts to billion USD
+    df[['GDP (Million USD)']] = np.round(df[['GDP (Million USD)']], 2)  #rounds off to 2 decimal places
+    df.rename(columns = {'GDP (Million USD)' : 'GDP (Billion USD)'})
 
+    df.to_csv('largest economies.csv')
 
-df['GDP (Million USD)'] = df['GDP (Million USD)'].astype(int)   #column's datatype is converted to int
+    print(df)
 
-df['GDP (Million USD)'] = (df[['GDP (Million USD)']]/1000)  #converts to billion USD
-
-df[['GDP (Million USD)']] = np.round(df[['GDP (Million USD)']], 2)  #rounds off to 2 decimal places
-
-df.rename(columns = {'GDP (Million USD)' : 'GDP (Billion USD)'})
-
-
-df.to_csv('largest economies.csv')
-
-print(df)
+highest_gdp(URL)
